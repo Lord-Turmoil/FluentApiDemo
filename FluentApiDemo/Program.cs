9e3b1d715e666f9a027bc7a5300711ae0482feb0
Lord Turmoil <tony-turmoil@outlook.com>
2024-04-01 09:37:01 +0800

    update: main executable description

--- FluentApiDemo/Program.cs
+++ FluentApiDemo/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace FluentApiDemo
+﻿// Copyright (C) 2018 - 2024 Tony's Studio. All rights reserved.
+
+namespace FluentApiDemo;
+
+class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("""
+                          Copyright (C) 2024 Tony's Studio, All rights reserved.
+                          ---------------------------------------------------------
+                          
+                          This is a demo of building Fluent API in C#.
+                          Here we have a simple example a SQL table builder.
+                          
+                          In "Basic/", we have a basic implementation of a table builder, but
+                          it will allow you to build invalid tables which has no column.
+                          
+                          In "Advanced/", we have an advanced implementation of a table builder, 
+                          which will prevent you from building invalid tables.
+                          
+                          The Test project contains tests for both implementations.
+                          
+                          Enjoy! :)
+                          """);
     }
 }
